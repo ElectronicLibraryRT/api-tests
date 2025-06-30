2e851d684dbb3ac0463c70604d618c6cb0a85d41
Сергей Воробьёв <sergey.vorobev_2004@mail.ru>
2025-06-30 07:26:26 +0300

    added authors endpoints tests

---
+++ src/tests/api/author/test_get_authors.py
@@ -0,0 +1,69 @@
+import pytest
+import requests
+from src.core.models import Author, Book
+from src.core.session import session_maker
+
+BASE_URL = "http://elibrary.ddns.net"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def init_db():
+    with session_maker() as session:
+        authors = [
+            Author(
+                id=1,
+                name="Федор Достоевский",
+                books=[
+                    Book(id=1, title="Преступление и наказание", publication_date="1866"),
+                    Book(id=2, title="Игрок", publication_date="1866"),
+                    Book(id=3, title="Идиот", publication_date="1869")
+                ]
+            ),
+            Author(
+                id=2,
+                name="Лев Толстой",
+                books=[
+                    Book(id=4, title="Война и мир", publication_date="1869"),
+                    Book(id=5, title="Анна Каренина", publication_date="1877")
+                ]
+            ),
+            Author(
+                id=3,
+                name="Антон Чехов",
+                books=[
+                    Book(id=6, title="Вишневый сад", publication_date="1904")
+                ]
+            ),
+            Author(
+                id=4,
+                name="Иван Тургенев",
+                books=[]
+            )
+        ]
+
+        session.add_all(authors)
+        session.commit()
+
+
+@pytest.mark.parametrize(
+    "params, expected_result",
+    [
+        ({}, ["Федор Достоевский", "Лев Толстой", "Антон Чехов", "Иван Тургенев"]),
+        ({"limit": 2}, ["Федор Достоевский", "Лев Толстой"]),
+        ({"offset": 1, "limit": 2}, ["Лев Толстой", "Антон Чехов"]),
+        ({"starts_with": "Л"}, ["Лев Толстой"]),
+        ({"starts_with": "И"}, ["Иван Тургенев"]),
+        ({"starts_with": "Ан"}, ["Антон Чехов"]),
+        ({"starts_with": "Алекс"}, []),
+        ({"offset": 3}, ["Иван Тургенев"]),
+    ]
+)
+def test_get_authors(params, expected_result: list[str]):
+    url = f"{BASE_URL}/authors"
+    response = requests.get(url, params=params)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    author_names = [author["name"] for author in data]
+    assert author_names == expected_result
